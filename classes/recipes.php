761ab5c11999f5e4beacf229324405d67fa0a355
Gus <gusta1296@gmail.com>
2021-03-01 08:16:03 -0800

    left at static methods

--- classes/recipes.php
+++ classes/recipes.php
@@ -3,11 +3,11 @@
 class Recipe{
 
     private $title;
-    public $ingredients = array();
-    public $instructions = array();
-    public $tag = array();
-    public $yield;
-    public $source = "Alena Holligan";
+    private $ingredients = array();
+    private $instructions = array();
+    private $tag = array();
+    private $yield;
+    private $source = "Alena Holligan";
 
     private $measurements = array(
         "tsp",
@@ -48,6 +48,38 @@ class Recipe{
 
     public function getIngredients(){
         return $this->ingredients;
+    }
+
+    public function addInstructions($string){
+        $this->instructions[] = $string;
+    }
+
+    public function getInstructions(){
+        return $this->instructions;
+    }
+
+    public function addTags($tag){
+        $this->tags[] = strtolower($tag);
+    }
+
+    public function getTags(){
+        return $this->tags;
+    }
+
+    public function setYield($yield){
+        $this->yield = $yield;
+    }
+
+    public function getYield(){
+        return $this->yield;
+    }
+
+    public function setSource($source){
+        $this->source = ucwords($source);
+    }
+
+    public function getSource(){
+        return $this->source;
     }
 
     public function displayRecipe(){
--- cookbook.php
+++ cookbook.php
@@ -5,19 +5,34 @@ include "classes/recipes.php";
 $ln = "\r\n";
 
 $recipe1 = new Recipe();
-$recipe1->source = "Grandma Holligan";
+$recipe1->setSource("Grandma Holligan");
 $recipe1->setTitle("my first recipe");
 $recipe1->addIngredient("egg", 1);
 $recipe1->addIngredient("flour", 2, "cup");
 
 $recipe2 = new Recipe();
-$recipe2->source = "Betty Crocker";
+$recipe2->setSource("Betty Crocker");
 $recipe2->setTitle("my second recipe");
 
 echo $recipe1->getTitle();
 
-// var_dump($recipe1->getIngredients());
-
 foreach($recipe1->getIngredients() as $ing){
     echo "\n" . $ing["amount"] . " " . $ing["measure"] . " " . $ing["item"];
 }
+
+$recipe1->addInstructions("This is my first instruction");
+$recipe2->addInstructions("This is my second instruction");
+
+echo $ln . implode($recipe1->getInstructions());
+echo $ln . implode($recipe2->getInstructions());
+
+$recipe1->addTags("Breakfast");
+$recipe2->addTags("Main Course");
+
+echo $ln . implode(", ", $recipe1->getTags());
+echo $ln . implode(", ", $recipe2->getTags());
+
+$recipe1->setYield("6 servings");
+
+echo $ln . $recipe1->getYield();
+echo $ln . $recipe1->getSource();
